bedbf0b154aba7b0f042a1815cef0374b52b48c7
Jude-Hageman <judehageman@gmail.com>
2026-04-29 12:45:14 -0400

    Moved over my game from lab 6.

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -3,6 +3,7 @@ class DoublyNode:
         self.value = value
         self.next = None
         self.prev = None
+        self.size = 0
 
 class LinkedList:
     def __init__(self):
@@ -13,14 +14,49 @@ class LinkedList:
         new_node = DoublyNode(value)
         if not self.head:
             self.head = self.tail = new_node
+            self.size = 1
         else:
             new_node.prev = self.tail
             self.tail.next = new_node
             self.tail = new_node
+            self.size += 1
+
+    def add_first(self, value):
+        """Adds an element to the beginning of the list. O(1)"""
+        new_node = DoublyNode(value)
+        if self.is_empty():
+            self.head = self.tail = new_node
+            self.size = 1
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.size += 1
 
     def remove_first(self):
         if not self.head: return None
         val = self.head.value
         self.head = self.head.next
-        if self.head: self.prev = None
+        self.size -= 1
+        if self.head: self.head.prev = None
         return val
+    
+    def remove_last(self):
+        if not self.tail: return None
+        val = self.tail.value
+        self.tail = self.tail.prev
+        self.size -= 1
+        if self.tail: self.tail.next = None
+        return val
+    
+    def is_empty(self):
+        return self.size == 0
+    
+    def __str__(self):
+        nodes = []
+        curr = self.head
+        while curr:
+            nodes.append(str(curr.value))
+            curr = curr.next
+        return " <-> ".join(nodes)
+    
